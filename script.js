eeb23fd6f6fa53f3721eb122bcdca7da0516d81e
ngalvin1991 <117473760+ngalvin1991@users.noreply.github.com>
2023-01-10 18:21:09 +0000

    adjusted .js file as realised the cancel button was not removing the characters that were not to be included, added confirm and if sections to generate password.

--- script.js
+++ script.js
@@ -1,13 +1,18 @@
-var charArr = [];
+//added variables for the code below
+var minPasswordLength = "";
+var includeNum;
+var includeChar;
+var includeUpper;
+var includeLower;
 
 //Included the variables below to state the minimum and maximum characters required. 
-var minPasswordLength = 10
-var maxPasswordLength = 64
+var minPasswordLength = "";
+
 
 
 // Array of special characters to be included in password
 var specialCharacters = [
-  '@','%','+','\\','/',"'",'!','#','$','^','?',':',',',')','(','}','{',']','[','~','-','_','.'
+  '@', '%', '+', '\\', '/', "'", '!', '#', '$', '^', '?', ':', ',', ')', '(', '}', '{', ']', '[', '~', '-', '_', '.'
 ];
 
 // Array of numeric characters to be included in password
@@ -15,74 +20,72 @@ var numericCharacters = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
 
 // Array of lowercase characters to be included in password
 var lowerCasedCharacters = [
-  'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'
+  'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'
 ];
 
 // Array of uppercase characters to be included in password
 var upperCasedCharacters = [
-  'A', 'B','C', 'D', 'E','F','G', 'H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'
+  'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'
 ];
 
-// Assignment Code: 
-var generateBtn = document.querySelector("#generate");
-
-// Add event listener to generate button
-generateBtn.addEventListener('click', writePassword);
-
-//Write password to the #password input
-function writePassword() {
-  var mainPrompts = userPrompts(); // this will return true or false.
-  var passwordText = document.querySelector("#password");
-
-  if (userPrompts) {
-    var password = generatePassword();
-    passwordText.value = password;
-  } else {
-    passWTest.value = "";
-  }
-  
-}
 
 //This section asks for the users input, what type of characters they would like to use. 
 function generatePassword() {
-  var userPassword = "";
-// Function for getting a random element from an array
-  for (var i = 0; i < minPasswordLength; i++) {
-    var randomPass = Math.floor(Math.random() * charArr.length);
-    userPassword = userPassword + charArr[randomPass];
- }
- return userPassword;
 
-}
-   
-function userPrompts (){
-  charArr = [];
+  var minPasswordLength = parseInt(prompt("Let's get you a new password! Choose between 10 & 64 characters :) "));
 
-  minPasswordLength = parseInt(prompt("Let's get you a new password! Choose between 10 & 64 characters :) "));
-
-  if (isNaN(minPasswordLength) || minPasswordLength < 10 || minPasswordLength > 64){
+  //checks to see if password is below or above min or max lengths
+  while (minPasswordLength <= 10 || minPasswordLength >= 64) {
     alert("Please use numbers between 10 & 64, try again!");
-    return false;
+    var minPasswordLength = (prompt("How many characters would you like in your password?"));
   }
 
-  if (confirm("Do you want your password to contain numbers?")); {
-    charArr = charArr.concat(numericCharacters);
+  //Confirmations of what the users wants to input
+  var includeNum = confirm("Would you like to include numbers?");
+  var includeChar = confirm("Would you like to include special characters?");
+  var includeUpper = confirm("Would you like to include uppercased characters?");
+  var includeLower = confirm("Would you like to include lowercased characters?");
+
+  while (includeNum === false && includeChar === false && includeUpper === false && includeLower === false) {
+    alert("You must select at least one option!");
   }
 
-  if (confirm("Do you want your password to contain special characters?")); {
-    charArr = charArr.concat(specialCharacters);
+  //this grabs the characters from the character variables at the top of the page.
+  var pwCharacters = []
+
+  if (includeNum) {
+    pwCharacters = pwCharacters.concat(numericCharacters);
   }
 
-  if (confirm("Do you want your password to contain uppercased characters?")); {
-    charArr = charArr.concat(upperCasedCharacters); 
+  if (includeChar) {
+    pwCharacters = pwCharacters.concat(specialCharacters);
   }
 
-  if (confirm("Do you want your password to contain lowercased characters?")); {
-    charArr = charArr.concat(lowerCasedCharacters);
-   
+  if (includeUpper) {
+    pwCharacters = pwCharacters.concat(upperCasedCharacters);
   }
-  return true;
+
+  if (includeLower) {
+    pwCharacters = pwCharacters.concat(lowerCasedCharacters);
+  }
+
+
+  // Function for getting a random element from an array
+  var userPassword = "";
+  for (var i = 0; i < minPasswordLength; i++) {
+    var userPassword = userPassword + pwCharacters[Math.floor(Math.random() * pwCharacters.length)];
+  }
+  return userPassword;
 }
 
+//Write password to the #password input
+function writePassword() {
+  var password = generatePassword(); // 
+  var passwordText = document.querySelector("#password");
 
-  
+  passwordText.value = password;
+}
+
+//this prompts the user to input there choices once the generate button has been clicked. 
+document.querySelector("#generate").addEventListener("click", writePassword);
+
